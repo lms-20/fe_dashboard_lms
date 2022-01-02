421b43ac16b0ab72e96160fc268fd1d4dc500d47
rizalmaulana32 <rizalmaulana28498@gmail.com>
2022-01-03 01:05:38 +0700

    fix onSubmit (migrating form using react-hook-form)

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -4,73 +4,14 @@
 import bgImage from '../../../assets/undraw_resume_re_hkth.svg';
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Register = props => {
-    const initialValues = {
-        fullname: "",
-        emailAddress: "",
-        age: "",
-        password: "",
-        checkPassword: "",
-        occupation: "",
-        phoneNumber: ""
-    }
-    const [formData, setFormData] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-        setFormErrors(validate(formData));
-
-        if (Object.keys(formErrors).length !== 0) {
-            console.log(formErrors);
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (Object.keys(formErrors).length === 0) {
-            console.log(formData);
-        }
-        console.log("there's errors")
-    }
-
-    const validate = (values) => {
-        const errors = {};
-        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-        if (!values.fullname) {
-            errors.fullname = "Fullname is required!"
-        }
-        if (!values.emailAddress) {
-            errors.emailAddress = "Email Address is required!"
-        } else if (!emailRegex.test(values.emailAddress)) {
-            errors.emailAddress = "Email not valid!"
-        }
-        if (!values.fullname) {
-            errors.fullname = "Fullname is required!"
-        }
-        if (!values.age) {
-            errors.age = "Age is required!"
-        }
-        if (!values.password) {
-            errors.password = "Password is required!"
-        }
-        if (!values.checkPassword) {
-            errors.checkPassword = "Check Password is required!"
-        }
-        if (!values.occupation) {
-            errors.occupation = "Occupation is required!"
-        }
-        if (!values.phoneNumber) {
-            errors.phoneNumber = "Phone Number is required!"
-        }
-        return errors;
+    const onSubmit = (data) => {
+        console.log(data)
     }
 
     return (
@@ -80,73 +21,76 @@ const Register = props => {
             <br />
             <div className="flex">
                 <div className="flex-auto w-64 border-black mx-5">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Full Name</span>
                             </label>
-                            <input type="text" id='fullname' name='fullname' placeholder="Full Name" className={!formErrors.fullname ? 'input' : ' input input-error'} value={formData.fullname} onChange={handleChange} />
+                            <input type="text" id='fullname' name='fullname' placeholder="Full Name" className={!errors.fullname?.type ? 'input' : 'input input-error'} {...register("fullname", { required: true })} />
                             <div className="label">
-                                <span className='text-red-500 text-sm'>{formErrors.fullname}</span>
+                                <span className='text-red-500 text-sm'>{errors.fullname?.type === "required" && "Fullname required"}</span>
                             </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email Address</span>
                             </label>
-                            <input type="text" id='emailAddress' name='emailAddress' placeholder="Email Address" className={!formErrors.emailAddress ? 'input' : ' input input-error'} value={formData.emailAddress} onChange={handleChange} />
+                            <input type="text" id='emailAddress' name='emailAddress' placeholder="Email Address" className={!errors.emailAddress?.type ? 'input' : 'input input-error'} {...register("emailAddress", { required: true, pattern: emailRegex })} />
                             <div className="label">
-                                <span className='text-red-500 text-sm'>{formErrors.emailAddress}</span>
+                                <span className='text-red-500 text-sm'>
+                                    {errors.emailAddress?.type === "required" && "Email Address required"}
+                                    {errors.emailAddress?.type === "pattern" && "Invalid Email Address"}
+                                </span>
                             </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Age</span>
                             </label>
-                            <input type="text" id='age' name='age' placeholder="Age" className={!formErrors.age ? 'input' : ' input input-error'} value={formData.age} onChange={handleChange} />
+                            <input type="text" id='age' name='age' placeholder="Age" className={!errors.age?.type ? 'input' : 'input input-error'} {...register("age", { required: true })} />
                             <div className="label">
-                                <span className='text-red-500 text-sm'>{formErrors.age}</span>
+                                <span className='text-red-500 text-sm'>{errors.age?.type === "required" && "Age required"}</span>
                             </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" id='password' name='password' placeholder="Password" className={!formErrors.password ? 'input' : ' input input-error'} value={formData.password} onChange={handleChange} />
+                            <input type="text" id='password' name='password' placeholder="Password" className={!errors.password?.type ? 'input' : 'input input-error'} {...register("password", { required: true })} />
                             <div className="label">
-                                <span className='text-red-500 text-sm'>{formErrors.password}</span>
+                                <span className='text-red-500 text-sm'>{errors.password?.type === "required" && "Password required"}</span>
                             </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Check Password</span>
                             </label>
-                            <input type="text" id='checkPassword' name='checkPassword' placeholder="Check Password" className={!formErrors.checkPassword ? 'input' : ' input input-error'} value={formData.checkPassword} onChange={handleChange} />
+                            <input type="text" id='checkPassword' name='checkPassword' placeholder="Check Password" className={!errors.checkPassword?.type ? 'input' : 'input input-error'} {...register("checkPassword", { required: true })} />
                             <div className="label">
-                                <span className='text-red-500 text-sm'>{formErrors.checkPassword}</span>
+                                <span className='text-red-500 text-sm'>{errors.checkPassword?.type === "required" && "CheckPassword required"}</span>
                             </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Occupation</span>
                             </label>
-                            <select id='occupation' name='occupation' className={!formErrors.occupation ? 'select select-bordered w-full max-w-xs' : ' select select-bordered w-full max-w-xs select-error'} value={formData.occupation} onChange={handleChange}>
+                            <select id='occupation' name='occupation' className={!errors.occupation?.type ? 'select select-bordered w-full max-w-xs' : 'select select-bordered w-full max-w-xs select-error'} {...register("occupation", { required: true })}>
                                 <option value="none" disabled selected>Choose your superpower</option>
                                 <option value="tele">telekinesis</option>
                                 <option value="time">time travel</option>
                                 <option value="invi">invisibility</option>
                             </select>
                             <div className="label">
-                                <span className='text-red-500 text-sm'>{formErrors.occupation}</span>
+                                <span className='text-red-500 text-sm'>{errors.occupation?.type === "required" && "Occupation required"}</span>
                             </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Phone Number</span>
                             </label>
-                            <input type="text" id='phoneNumber' name='phoneNumber' placeholder="Phone Number" className={!formErrors.phoneNumber ? 'input' : ' input input-error'} value={formData.phoneNumber} onChange={handleChange} />
+                            <input type="text" id='phoneNumber' name='phoneNumber' placeholder="Phone Number" className={!errors.phoneNumber?.type ? 'input' : 'input input-error'} {...register("phoneNumber", { required: true })} />
                             <div className="label">
-                                <span className='text-red-500 text-sm'>{formErrors.phoneNumber}</span>
+                                <span className='text-red-500 text-sm'>{errors.phoneNumber?.type === "required" && "PhoneNumber required"}</span>
                             </div>
                         </div>
                         <div className="form-control">
