86267f21fa8716e431c361416ee990c66fbcace2
Rizky Ramadhan <rizkysr90@gmail.com>
2022-01-06 15:43:32 +0700

    slicing ui from figma

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PermanentDrawer from './components/PermanentDrawer/PermanentDrawer';
 import { Routes, Route } from 'react-router-dom';
 import MyClass from './pages/MyClass/MyClass';
 import Transaction from './pages/Transaction/Transaction';
+import UserSettings from './pages/UserSettings/UserSettings';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route element={<PermanentDrawer />}>
           <Route path="/myclass" element={<MyClass />} />
           <Route path="/transactions" element={<Transaction />} />
+          <Route path="/settings" element={<UserSettings />} />
         </Route>
       </Routes>
     </div>
