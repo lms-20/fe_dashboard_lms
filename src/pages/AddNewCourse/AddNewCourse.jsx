2f3e4c20dc6921ff6e0125c4d75d5ffaa0347665
rizalmaulana32 <rizalmaulana28498@gmail.com>
2022-01-12 08:01:16 +0700

    tes

---
+++ src/pages/AddNewCourse/AddNewCourse.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AddNewCourse = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+AddNewCourse.propTypes = {
+
+}
+
+export default AddNewCourse
