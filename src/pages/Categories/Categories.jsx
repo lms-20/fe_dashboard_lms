58ad7e4a0af911c7b78db601866bf80a5f65afc2
rizalmaulana32 <rizalmaulana28498@gmail.com>
2022-01-20 09:59:50 +0700

    succes separate load data categories on landing page and all categories

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -2,12 +2,34 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CategoriesCard from '../../components/CategoriesCard/CategoriesCard';
 import Navbar from '../../components/Navbar/Navbar';
 
 
 const Categories = () => {
+    const ApiSections = `https://61e62635ce3a2d0017358fa7.mockapi.io/category`;
+    const [categories, setCategories] = useState([]);
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(ApiSections)
+            .then(response => {
+                response?.data.forEach(dataSections => {
+                    setCategories(
+                        prevstate => [...prevstate, dataSections]
+                    )
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, []);
+
+    console.log(categories)
+
     return (
         <div className='min-h-screen  bg-neutral-content'>
             <div className='w-11/12 mx-auto my-10'>
